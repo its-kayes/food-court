dac09bb45ebee21f66277b6f2606a749e9c9fd22
ATISH01 <ab.atish.ab@gmail.com>
2022-07-01 23:12:15 +0600

    commit

--- components/Home/HomeMain.js
+++ components/Home/HomeMain.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Banner } from './Banner'
+import { WhyChooseUs } from './WhyChooseUs'
 
 export const HomeMain = () => {
   return (
     <div>
       <Banner> </Banner>
+      <WhyChooseUs></WhyChooseUs>
     </div>
   )
 }
