c65f31624357407c3beb3c90de64c9dc7cbe9150
ABU-ISA-BISWAS <bmabuisa.cse@gmail.com>
2022-07-01 10:10:09 +0600

    abuisa

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <i className='text-center text-4xl font-bold'> Team Work  </i>
+      <i className='text-center text-4xl font-bold'> Team Work food court </i>
     </div>
   )
 }
