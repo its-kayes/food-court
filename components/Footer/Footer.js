9df8a37e990c565f1270e0ff19988c005bd153ec
monirakibul <monirakibul@gmail.com>
2022-07-06 18:32:37 +0600

    simple login & registration added

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { AiFillFacebook, AiFillInstagram, AiFillTwitterCircle, AiFillLinkedin } from 'react-icons/ai';
 import Link from 'next/link';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const Footer = () => {
   return (
@@ -53,6 +55,7 @@ export const Footer = () => {
             <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
           </div>
         </div>
+        <ToastContainer />
       </footer>
     </>
   )
