41ec9fc29baba3d91000ec36da1f86e6150e5110
its-kayes <kayes.ek8@gmail.com>
2022-07-15 22:14:25 +0600

    Blog Page deploy solve 2

--- pages/blogs/Blog.js
+++ pages/blogs/Blog.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 function Index({ food }) {
-  const { name, img } = food;
+  const { img } = food;
   console.log(food);
   return (
     <div>
       <div className="card card-compact w-96 bg-base-100 shadow-xl mx-auto">
         <figure><img src={img} alt="Shoes" /></figure>
         <div className="card-body">
-          <h2 className="card-title">{name}</h2>
+          <h2 className="card-title">Greek yogurt breakfast bowls with toppings</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
 
         </div>
